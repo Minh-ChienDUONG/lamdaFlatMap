d9f49020eb39662c3ef05b5daaa11ce4f2da51ba
Minh-ChienDUONG <46671487+Minh-ChienDUONG@users.noreply.github.com>
2020-12-09 12:00:31 +0100

    flat map

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main{
+    List<String> someBingoNumbers = Arrays.asList(
+            "N40","N36",
+            "N12","N25"
+    );
+
+
+
+
+}
